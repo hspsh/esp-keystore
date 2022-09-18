4dbdd01e3c90dd3a5b4815ff4790d489b1655159
psuwala <oneat@vp.pl>
2022-09-19 01:51:34 +0200

    checkpoint before the best thing you have ever seen

--- .ccls-cache/@home@psuwala@Projects@ArduinoToMQTT/src@main.cpp
+++ .ccls-cache/@home@psuwala@Projects@ArduinoToMQTT/src@main.cpp
@@ -1,16 +1,53 @@
-#include <Arduino.h>
 #include "Button.h"
 #include "RestClient.h"
+#include <Arduino.h>
 #include <AsyncMqttClient.h>
 #include <ESP8266WiFi.h>
 #include <string>
 
 RestClient client = RestClient("banana.at.hsp.net.pl", 8000);
-
 AsyncMqttClient mqttClient;
-Button buttonA(14, INPUT);
-Button buttonB(13, INPUT);
-Button buttonC(12, INPUT);
+
+class MqNode {
+public:
+  virtual void onInit(String topic);
+};
+
+class MqBranch : public MqNode {
+  const char *name;
+  MqNode mqNode;
+  String currentTopic = "";
+
+public:
+  MqBranch(const char *name, MqNode mqNode) : name(name), mqNode(mqNode) {}
+
+  void onInit(String topic) {
+    topic.concat("/");
+    topic.concat(name);
+    mqNode.onInit(currentTopic.c_str());
+  }
+};
+
+class MqButton : public MqNode {
+  Button button;
+
+public:
+  MqButton(uint8 port) : button(port, INPUT) {}
+
+  void onInit(String topic) {
+    mqttClient.publish(topic.c_str(), 0, true,
+                       button.isPressed() ? "YES" : "NO");
+    button.onChange([topic, this]() {
+      mqttClient.publish(topic.c_str(), 0, true,
+                         button.isPressed() ? "YES" : "NO");
+    });
+  }
+};
+
+
+MqNode buttonA = MqButton(14);
+MqNode buttonB = MqButton(13);
+MqNode buttonC = MqButton(12);
 
 bool connected = false;
 
@@ -39,26 +76,16 @@ void setup() {
   topicC.concat(deviceTopic);
   topicC.concat("C");
 
+  Serial.println("XD");
 
-  mqttClient.onConnect([&](bool b) {
+  mqttClient.onConnect([](bool b) {
     Serial.println("CONNECTED");
-    mqttClient.publish(topicA.c_str(), 0, true, buttonA.isPressed() ? "YES" : "NO");
-    mqttClient.publish(topicB.c_str(), 0, true, buttonB.isPressed() ? "YES" : "NO");
-    mqttClient.publish(topicC.c_str(), 0, true, buttonC.isPressed() ? "YES" : "NO");
-    connected = true;
+    buttonA.onInit(topicA);
+    buttonB.onInit(topicB);
+    buttonC.onInit(topicC);
+    Serial.println("IT IS DONE");
   });
   mqttClient.connect();
-
-  buttonA.onChange([]() {
-    mqttClient.publish(topicA.c_str(), 0, true, buttonA.isPressed() ? "YES" : "NO");
-  });
-  buttonB.onChange([]() {
-    mqttClient.publish(topicB.c_str(), 0, true, buttonB.isPressed() ? "YES" : "NO");
-  });
-  buttonC.onChange([]() {
-    mqttClient.publish(topicC.c_str(), 0, true, buttonC.isPressed() ? "YES" : "NO");
-  });
 }
 
-void loop() {
-}
+void loop() {}
